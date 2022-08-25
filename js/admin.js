0c8fc1a7e19fd95a022329821ed5fd89f5080808
Luciano <lucasacci@hotmail.com>
2022-08-24 23:41:41 -0300

    Finishing delete of movies

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,7 @@
 import { Movie } from "./crearPelis.js";
 import { validarCode, validarDescription, validarTitle, validarUrl } from './helpers.js';
 
-const movieList = JSON.parse(localStorage.getItem('listaPeliculasKey')) || []; 
+let movieList = JSON.parse(localStorage.getItem('listaPeliculasKey')) || []; 
 
 let title = document.getElementById('title');
 let description = document.getElementById('description');
@@ -20,6 +20,40 @@ url.addEventListener('blur', ()=>{validarUrl(url)})
 
 // the library uuidv4 provides an unique id to the movie 
 
+
+
+const cargaInicial = () =>{
+    
+    if(movieList.length > 0){
+        movieList.forEach((movieItem)=>{ crearFilas(movieItem) })
+    }
+    
+}
+
+const crearFilas = (movieItem) =>{
+// draw a tr
+
+    let movieTable = document.querySelector('#movieTable');
+
+    movieTable.innerHTML += `<tr>
+    <th scope="row">${movieItem.code}</th>
+    <td>${movieItem.title}</td>
+    <td>
+      ${movieItem.description}
+    </td>
+    <td>${movieItem.img}</td>
+    <td>${movieItem.genre}</td>
+    <td>
+      <button class="btn btn-warning">
+        Editar
+      </button>
+      <button class="btn btn-danger" onClick='deleteMovie("${movieItem.code}")'>
+        Borrar
+      </button>
+    </td>
+  </tr>`
+
+}
 
 const createMovie = () => {
 
@@ -41,7 +75,7 @@ const saveMovie = (e) => {
             cleanForm();
             console.log(movieList)
             modalAdmin.hide();
-        
+
             Swal.fire(
                 'Pelicula creada con exito!'
               )
@@ -58,6 +92,7 @@ const cleanForm = () => {
 
 }
 
+cargaInicial();
 createButton.addEventListener('click', createMovie);
 form.addEventListener('submit', saveMovie);
 
@@ -68,3 +103,60 @@ const saveMovieLs = () => {
 
 
 }
+
+window.deleteMovie = function (code) { 
+    console.log(code);
+
+    let movieListCopy = movieList.filter((movieItem) => movieItem.code != code);
+    movieList = movieListCopy;
+
+    saveMovieLs();
+
+    borrarTabla();
+    cargaInicial();
+    cargaInicial2();
+}
+
+function borrarTabla(){
+    let movieTable = document.querySelector('#movieTable');
+    movieTable.innerHTML = '';
+}
+
+
+
+
+
+// const cargaInicial2 = () =>{
+    
+//   if(movieList.length > 0){
+//       movieList.forEach((movieItem)=>{ crearCard(movieItem) })
+//   }
+  
+// }
+
+// cargaInicial2();
+// form.addEventListener('submit', crearCard);
+// console.log(crearCard);
+
+// function crearCard(movieItem){
+
+
+//   let padre = document.querySelector("#seccionXD");
+
+//     let card = document.createElement('aside');
+//     card.className = 'col-12 col-md-4 col-lg-3 my-3'
+    
+//     card.innerHTML += `<aside class="col-12 col-md-4 col-lg-3 my-3">
+//     <div class="card">
+//       <img src="${movieItem.img}" class="card-img-top" alt="img">
+//       <div class="card-body">
+//         <h5 class="card-title ">${movieItem.title}<span class="badge text-bg-danger rounded-pill">Nuevo</span></h5>
+//         <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#modalMedialuna">Ver mas</button>
+//       </div>
+//     </div>
+//   </aside>`
+    
+//     padre.appendChild(card);
+  
+
+// }
--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
 
-let code = document.getElementById('code');
-let title = document.getElementById('title');
-let description = document.getElementById('description');
-let url = document.getElementById('url');
-let form = document.getElementById('modalForm');
-let padre = document.querySelector("#section1");
+// let code = document.getElementById('code');
+// let title = document.getElementById('title');
+// let description = document.getElementById('description');
+// let url = document.getElementById('url');
+// let form = document.getElementById('modalForm');
+// let padre = document.querySelector("#seccionXD");
 
 
 
-let n = [];
+// let n = [];
 
-const createCard = (e)=> {
-  e.preventDefault();
+// const createCard = (e)=> {
+//   e.preventDefault();
 
-  if(validarCode(code) && validarTitle(title) && validarDescription(description) && validarUrl(url)){
+//   if(validarCode(code) && validarTitle(title) && validarDescription(description) && validarUrl(url)){
 
-    let card = document.createElement('aside');
-    card.className = 'col-12 col-md-4 col-lg-3 my-3'
+//     let card = document.createElement('aside');
+//     card.className = 'col-12 col-md-4 col-lg-3 my-3'
     
-    card.innerHTML = `<div class="card">
-    <img src="${url.value}" class="card-img-top" alt="img" id="${code.value}">
-    <div class="card-body">
-    <h5 class="card-title">${title.value}<span class="badge text-bg-danger rounded-pill mx-2">Nuevo</span></h5>
-    <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#modalMedialuna">Ver mas</button>
-    </div>
-    </div>`
+//     card.innerHTML = `<div class="card">
+//     <img src="${url.value}" class="card-img-top" alt="img" id="${code.value}">
+//     <div class="card-body">
+//     <h5 class="card-title">${title.value}<span class="badge text-bg-danger rounded-pill mx-2">Nuevo</span></h5>
+//     <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#modalMedialuna">Ver mas</button>
+//     </div>
+//     </div>`
     
     
-    padre.appendChild(card);
+//     padre.appendChild(card);
   
-    n.push(code.value);
+//     n.push(code.value);
     
-    form.reset();
-  }else{
-    alert(`Ingrese todos los datos`);
-  }
+//     form.reset();
+//   }else{
+//     alert(`Ingrese todos los datos`);
+//   }
   
-}
+// }
 
-form.addEventListener('submit', createCard);
+// form.addEventListener('submit', createCard);
