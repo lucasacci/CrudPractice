9704bdcf280ff9211abe4d44bcbb6d5b2634ddd6
Luciano <lucasacci@hotmail.com>
2022-08-29 11:19:42 -0300

    editar y actualizar datos

--- js/admin.js
+++ js/admin.js
@@ -1,41 +1,53 @@
 import { Movie } from "./crearPelis.js";
-import { validarCode, validarDescription, validarTitle, validarUrl } from './helpers.js';
+import {
+  validarCode,
+  validarDescription,
+  validarTitle,
+  validarUrl,
+} from "./helpers.js";
 
-let movieList = JSON.parse(localStorage.getItem('listaPeliculasKey')) || []; 
+let movieList = JSON.parse(localStorage.getItem("listaPeliculasKey")) || [];
 
-let title = document.getElementById('title');
-let description = document.getElementById('description');
-let url = document.getElementById('url');
-let form = document.getElementById('modalForm');
-let createButton = document.getElementById('createMovie');
-let code = document.getElementById('code');
+let title = document.getElementById("title");
+let description = document.getElementById("description");
+let url = document.getElementById("url");
+let form = document.getElementById("modalForm");
+let createButton = document.getElementById("createMovie");
+let code = document.getElementById("code");
 
+let x = true; //if x == true i create a new movie else i update the movie data
 
-code.addEventListener('change', ()=>{validarCode(code)});
-title.addEventListener('blur', ()=>{validarTitle(title)})
-description.addEventListener('blur', ()=>{validarDescription(description)});
-url.addEventListener('blur', ()=>{validarUrl(url)})
+code.addEventListener("change", () => {
+  validarCode(code);
+});
+title.addEventListener("blur", () => {
+  validarTitle(title);
+});
+description.addEventListener("blur", () => {
+  validarDescription(description);
+});
+url.addEventListener("blur", () => {
+  validarUrl(url);
+});
 
- const modalAdmin = new bootstrap.Modal(document.getElementById('modal111'))
+const modalAdmin = new bootstrap.Modal(document.getElementById("modal111"));
 
-// the library uuidv4 provides an unique id to the movie 
+// the library uuidv4 provides an unique id to the movie
 
+const cargaInicial = () => {
+  if (movieList.length > 0) {
+    movieList.forEach((movieItem) => {
+      crearFilas(movieItem);
+    });
+  }
+};
 
+const crearFilas = (movieItem) => {
+  // draw a tr
 
-const cargaInicial = () =>{
-    
-    if(movieList.length > 0){
-        movieList.forEach((movieItem)=>{ crearFilas(movieItem) })
-    }
-    
-}
+  let movieTable = document.querySelector("#movieTable");
 
-const crearFilas = (movieItem) =>{
-// draw a tr
-
-    let movieTable = document.querySelector('#movieTable');
-
-    movieTable.innerHTML += `<tr>
+  movieTable.innerHTML += `<tr>
     <th scope="row">${movieItem.code}</th>
     <td>${movieItem.title}</td>
     <td>
@@ -44,94 +56,139 @@ const crearFilas = (movieItem) =>{
     <td>${movieItem.img}</td>
     <td>${movieItem.genre}</td>
     <td>
-      <button class="btn btn-warning">
+      <button class="btn btn-warning" onClick='editMovie("${movieItem.code}")'>
         Editar
       </button>
       <button class="btn btn-danger" onClick='deleteMovie("${movieItem.code}")'>
         Borrar
       </button>
     </td>
-  </tr>`
-
-}
+  </tr>`;
+};
 
 const createMovie = () => {
 
-    modalAdmin.show();
-    
-    code.value = uuidv4();
-    console.log(code.value)
-    
-}
+  x = true;
 
-const saveMovie = (e) => { 
-    e.preventDefault();
-    
-    if(validarTitle(title) && validarDescription(description) && validarUrl(url)){
-        
-            let newMovie = new Movie(code.value, title.value, description.value, url.value);
-            movieList.push(newMovie);
-            saveMovieLs();
-            cleanForm();
-            console.log(movieList)
-            modalAdmin.hide();
+  cleanForm();
 
-            Swal.fire(
-                'Pelicula creada con exito!'
-              )
+  modalAdmin.show();
 
+  code.value = uuidv4();
+};
+
+const saveMovie = (e) => {
+  e.preventDefault();
+
+
+  if (
+    validarTitle(title) &&
+    validarDescription(description) &&
+    validarUrl(url)
+  ) {
+    if(x ){
+
+
+      let newMovie = new Movie(
+        code.value,
+        title.value,
+        description.value,
+        url.value
+      );
+      movieList.push(newMovie);
+      saveMovieLs();
+      cleanForm();
+      console.log(movieList);
+      modalAdmin.hide();
+  
+      Swal.fire("Pelicula creada con exito!");
     }else{
-        alert('Ingrese datos correctos')
+      updateMovie();
     }
-}
+  } else {
+    alert("Ingrese datos correctos");
+  }
 
+  
+
+};
 
 const cleanForm = () => {
-
-    form.reset();
-
-}
+  form.reset();
+};
 
 cargaInicial();
-createButton.addEventListener('click', createMovie);
-form.addEventListener('submit', saveMovie);
-
+createButton.addEventListener("click", createMovie);
+form.addEventListener("submit", saveMovie);
 
 const saveMovieLs = () => {
+  localStorage.setItem("listaPeliculasKey", JSON.stringify(movieList));
+};
 
-    localStorage.setItem('listaPeliculasKey', JSON.stringify(movieList));
+window.deleteMovie = function (code) {
+  console.log(code);
 
+  let movieListCopy = movieList.filter((movieItem) => movieItem.code != code);
+  movieList = movieListCopy;
 
+  saveMovieLs();
+  borrarTabla();
+  cargaInicial();
+
+};
+
+function borrarTabla() {
+  let movieTable = document.querySelector("#movieTable");
+  movieTable.innerHTML = "";
 }
 
-window.deleteMovie = function (code) { 
-    console.log(code);
+window.editMovie = function(codex){
 
-    let movieListCopy = movieList.filter((movieItem) => movieItem.code != code);
-    movieList = movieListCopy;
+  x = false;
 
-    saveMovieLs();
+  let movie = movieList.find((movie)=>(movie.code)=== codex)
 
-    borrarTabla();
-    cargaInicial();
-    cargaInicial2();
+  modalAdmin.show();
+
+  code.value = movie.code;
+  title.value = movie.title;
+  description.value = movie.description;
+  url.value = movie.img;
 }
 
-function borrarTabla(){
-    let movieTable = document.querySelector('#movieTable');
-    movieTable.innerHTML = '';
+function updateMovie(){
+
+  let position = movieList.findIndex((movie)=>code.value === movie.code);
+  
+
+  movieList[position].title = title.value;
+  movieList[position].img = url.value;
+  movieList[position].description = description.value;
+
+  saveMovieLs();
+
+
+  borrarTabla();
+  cargaInicial();
+
+  Swal.fire(
+    'Pelicula Actualizada!',
+    'Datos actualizados con exito',
+    'success'
+  )
+
+  modalAdmin.hide();
+
+  cleanForm();
 }
-
-
-
 
 
 // const cargaInicial2 = () =>{
-    
+
 //   if(movieList.length > 0){
 //       movieList.forEach((movieItem)=>{ crearCard(movieItem) })
 //   }
-  
+
 // }
 
 // cargaInicial2();
@@ -140,12 +197,11 @@ function borrarTabla(){
 
 // function crearCard(movieItem){
 
-
 //   let padre = document.querySelector("#seccionXD");
 
 //     let card = document.createElement('aside');
 //     card.className = 'col-12 col-md-4 col-lg-3 my-3'
-    
+
 //     card.innerHTML += `<aside class="col-12 col-md-4 col-lg-3 my-3">
 //     <div class="card">
 //       <img src="${movieItem.img}" class="card-img-top" alt="img">
@@ -155,8 +211,7 @@ function borrarTabla(){
 //       </div>
 //     </div>
 //   </aside>`
-    
+
 //     padre.appendChild(card);
-  
 
 // }
